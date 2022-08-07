390b5b9a92f5eef62946510171b55af2fdc14c41
Graciela Romano <graci.romano@gmail.com>
2022-08-07 20:54:18 -0300

    Corregi los errores para que se muestre en pantalla tanto en pantalla grandes como en pequeñas.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import './Header.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faInfo, faBarsProgress, faDiagramProject, faCertificate} from '@fortawesome/free-solid-svg-icons'
+import { faInfo, faBarsProgress, faDiagramProject, faCertificate, faHome} from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-scroll'
 
 const Header = () => {
   return (
     <div className='header'>
-        <nav className="navbar navbar-expand-sm menu">
+        <nav className="navbar navbar-expand-lg menu">
             <div className="container-fluid">
                 <Link  className="logo" to='/'>GR </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" style={{color:'yellow'}}>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation" style={{color:'rgb(194, 145, 153)'}}>
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
                     <ul className="navbar-nav">
+                    <li className="nav-item">
+                            <Link className="nav-link"
+                                    to='home'
+                                    smooth={true}>
+                                    <FontAwesomeIcon icon = {faHome}/>Home
+                                </Link>
+                        </li>
                         <li className="nav-item">
                             <Link className="nav-link"
                                     to='aboutme'
-                                    span={true}
                                     smooth={true}>
                                     <FontAwesomeIcon icon = {faInfo}/>  Acerca de mi
                                 </Link>
                         </li>
                         <li className="nav-item">
                                 <Link className="nav-link" 
-                                    
                                     to='education'
-                                    span={true}
                                     smooth={true}
                                     >
                                     <FontAwesomeIcon icon = {faBarsProgress}/> Resumen
@@ -36,7 +40,6 @@ const Header = () => {
                         <li className="nav-item">
                             <Link className="nav-link" 
                                 to='projects'
-                                span={true}
                                 smooth={true}
                                 >
                                 <FontAwesomeIcon icon = {faDiagramProject}/> Proyectos
@@ -45,7 +48,6 @@ const Header = () => {
                         <li className="nav-item">
                             <Link className="nav-link" 
                                 to='certifications'
-                                span={true}
                                 smooth={true}
                                 >
                                 <FontAwesomeIcon icon = {faCertificate}/> Certificaciones
@@ -54,7 +56,7 @@ const Header = () => {
                     </ul>
                 </div>
             </div>
-        </nav>       
+        </nav>
     </div>
   )
 }
